33cdc23deb68739eac4a775ab96b91ca1210de79
fabianriewe <fabian@kyve.network>
2021-12-03 13:41:27 +0100

    chore: added second revision for day 3.1

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -30,3 +30,28 @@ gamma = int(gamma, 2)
 epsilon = int(epsilon, 2)
 
 print(gamma * epsilon)
+
+# revision 2
+gamma = ""
+epsilon = ""
+
+for bit_index in range(len(lines[0])):
+    zeros = 0
+    ones = 0
+    for line in lines:
+        if line[bit_index] == "0":
+            zeros += 1
+        else:
+            ones += 1
+
+    if zeros > ones:
+        gamma += "0"
+        epsilon += "1"
+    else:
+        gamma += "1"
+        epsilon += "0"
+
+gamma = int(gamma, 2)
+epsilon = int(epsilon, 2)
+
+print(gamma * epsilon)
